140bfcfae4c9078032a8caa71b38bb2eff73593d
ketakitankk <ketakitankk@gmail.com>
2023-02-04 20:06:14 +0530

    Update files

--- my-app/src/Components/Achievements.jsx
+++ my-app/src/Components/Achievements.jsx
@@ -28,11 +28,11 @@ const Achievements = () => {
       <img src={courses} alt="courses" className='course'></img>
       <div className='courses-undertaken'>
         <ul>
-          <li className='courses'><a className='link' href="https://www.udemy.com/certificate/UC-d930da32-5109-460b-b586-1a2bca61e351/">React - Beginner to Advanced + Redux </a></li>
-          <li className='courses'><a className='link' href="https://www.udemy.com/certificate/UC-bd61c5b5-294b-4391-bcf1-add84fdde4d8/">Mastering Data Structures and Algorithms in C</a></li>
-          <li className='courses'><a className='link' href="https://www.udemy.com/certificate/UC-1f5215d6-45b9-4579-a69d-4ecbe52fbb92/">Java Programming Masterclass </a></li>
-          <li className='courses'><a className='link' href="https://coursera.org/share/cabab08a05a3bd55e7543f3a8cc26e8d">AWS Fundamentals Specialization</a></li>
-          <li className='courses'><a className='link' href="https://coursera.org/share/e3cb81e78db54a2aa27146f2c28c2e7a">Microsoft Azure Fundamentals A-Z-900 examination specialization </a></li>
+          <li className='courses'><a className='course-link' href="https://www.udemy.com/certificate/UC-d930da32-5109-460b-b586-1a2bca61e351/">React - Beginner to Advanced + Redux </a></li>
+          <li className='courses'><a className='course-link' href="https://www.udemy.com/certificate/UC-bd61c5b5-294b-4391-bcf1-add84fdde4d8/">Mastering Data Structures and Algorithms in C</a></li>
+          <li className='courses'><a className='course-link' href="https://www.udemy.com/certificate/UC-1f5215d6-45b9-4579-a69d-4ecbe52fbb92/">Java Programming Masterclass </a></li>
+          <li className='courses'><a className='course-link' href="https://coursera.org/share/cabab08a05a3bd55e7543f3a8cc26e8d">AWS Fundamentals Specialization</a></li>
+          <li className='courses'><a className='course-link' href="https://coursera.org/share/e3cb81e78db54a2aa27146f2c28c2e7a">Microsoft Azure Fundamentals A-Z-900 examination specialization </a></li>
           <li className='courses'>Discrete Mathematics</li>
           <li className='courses'>Machine Learning & Mathematics for Machine Learning</li>
           <li className='courses'>Database Management Systems & SQL</li>
